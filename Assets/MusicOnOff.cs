0fa426819d6664063db107090f937251cfd24dc0
Ideae <zack.tibia@gmail.com>
2014-06-25 22:29:56 -0400

    music, score board on

---
+++ Assets/MusicOnOff.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicOnOff : MonoBehaviour {
+
+//	private bool musicOn = true;
+//	private AudioSource audio;
+//
+//
+//	// Use this for initialization
+//	void Start () {
+//		audio = gameObject.GetComponent<AudioSource>();
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		if (!audio.isPlaying )//&& musicOn
+//			audio.Play();
+//		else 
+//			audio.Stop();
+//
+//		if (Input.GetMouseButtonDown(0)){
+//			musicOn = !musicOn;
+//		}
+//	}
+}
