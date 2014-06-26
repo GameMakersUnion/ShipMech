3328c1327659e7a90c981d0dccaecfde7fc6401a
Ian Nastajus <ian.nastajus@gmail.com>
2014-06-25 20:10:50 -0400

    ET

--- Assets/ShipMovement.cs
+++ Assets/ShipMovement.cs
@@ -3,34 +3,25 @@ using System.Collections;
 
 public class ShipMovement : MonoBehaviour {
 
-	private float forwardSpeed = 0f;
+	private float forwardSpeed = 10f;
 	private float strafeSpeed = 5f;
-	private Plane plane;
-	private Transform plane2;
+	private Plane plane, plane2;
 
 	// Use this for initialization
 	void Start () {
-		plane = new Plane( Vector3.forward, 0 );
-		Debug.Log ( "" );
-		plane2 = transform.FindChild("PlaneForRaycasting");
+		plane = new Plane( Vector3.back, 0 );
+		plane2 = new Plane( Vector3.back, 0 );
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		transform.position = new Vector3( transform.position.x, transform.position.y, transform.position.z + forwardSpeed * Time.deltaTime );
+		Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z + forwardSpeed * Time.deltaTime );
 		//get mouse position on screen
 		//... get translated to world coords
 		//...find diff btwn ship and mouse position in world
 		// move that way
-		old ();
-		//planes();
-	}
-	void old()
-	{
-		Vector2 mousePos = Input.mousePosition;
-		Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3 (mousePos.x,mousePos.y));
-		Vector3 currentPos = new Vector3(transform.position.x, transform.position.y, 0);
-		transform.position = new Vector3(worldPos.x, worldPos.y, transform.position.z);
+		planes();
 	}
 	void planes()
 	{
@@ -40,6 +31,7 @@ public class ShipMovement : MonoBehaviour {
 		plane.distance = transform.position.z + 1f;
 		if ( plane.Raycast(ray, out dist)){
 			Vector3 point = ray.GetPoint(dist);
+
 			//Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3 (mousePos.x,mousePos.y,0));
 			Vector3 diff = point - transform.position; 
 			diff.z = 0;
@@ -47,5 +39,13 @@ public class ShipMovement : MonoBehaviour {
 			diff *= strafeSpeed * Time.deltaTime;
 			transform.position += diff;
 		}
+		plane2.distance = transform.position.z + 2f;
+		if (plane2.Raycast (ray, out dist))
+		{
+			Vector3 point = ray.GetPoint(dist);
+			Debug.Log(point);
+			transform.LookAt(point);
+		}
+
 	}
 }
